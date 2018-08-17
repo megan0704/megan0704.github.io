c9981bec397c622cba897c2160610bbce4f75eac
megan0704 <40831640+megan0704@users.noreply.github.com>
2018-08-17 13:09:54 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,9 +5,17 @@ function draw() {
   fill(value); 
   rect(25, 25, 50, 50); 
 } 
-function touchMoved() { 
-  value = value + 5; 
-  if (value > 255) { 
-    value = 0; 
-  } 
+// function touchMoved() { 
+//   value = value + 5; 
+//   if (value > 255) { 
+//     value = 0; 
+//   } 
+// }
+function touchStarted() { 
+  if (value === 0) { 
+    value = 255; } 
+  else { value = 0; 
+       } 
 }
+// function touchStarted() { 
+//   ellipse(mouseX, mouse
