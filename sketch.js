2f0d23f46fe6477321f837382d0ae4698af491eb
megan0704 <40831640+megan0704@users.noreply.github.com>
2018-08-17 15:06:29 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,45 +1,12 @@
-var posx,stop;
-var width,height;
-var value = 0;
-var birdx=0;
-
-function setup() {
-  width=200;
-  height=400;
-  createCanvas(width, height);
+var x=0;
+ 
+function setup() { 
+  createCanvas(400, 400);
+} 
+ 
+function draw() { 
   background(220);
-  posx=0;
-  stop=0;
+  //width和height是关键词，分别是Canvas的宽和高
+  x+=2;
+  ellipse(x,height/2,20,20);
 }
-
-function draw() {
-  background(220);
-  noStroke();
-  posx+=0.5;
-  birdx=posx+stop
-  fill(0);
-  ellipse(birdx,200,50);
-  ellipse(400-birdx,200,50);
-  //print(birdx);
-  //test touch
-  
-  rect(200,200,50,50);
-
-
-}
-
- function touchStart(){
-  // if(value ===0){
-    stop-=5;
-   
-    // print("b");
-  // }else{
-  //   stop=0;
-  //   value = 0;
-  // }
- }
-
-// function mousePressed(){
-//   stop-=5;
-//   //print(stop);
-// }
