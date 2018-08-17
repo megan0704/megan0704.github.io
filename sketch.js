0f7ab06d863fd324fcb46bc691bda7e53833c5eb
megan0704 <40831640+megan0704@users.noreply.github.com>
2018-08-17 13:03:52 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
-// Touch within the image to change // the value of the rectangle 
-var value = 0; 
+// Move your finger across the page
+// to change its value 
+var value = 0;
 function draw() { 
-  fill(value); rect(25, 25, 50, 50); } 
-function touchStarted() { 
-  if (value === 0) { value = 255; } 
-  else { value = 0; } 
-}
-function touchStarted() { 
-  ellipse(mouseX, mouseY, 5, 5); 
-  // prevent default return false; 
+  fill(value); 
+  rect(25, 25, 50, 50); 
+} 
+function touchMoved() { 
+  value = value + 5; 
+  if (value > 255) { 
+    value = 0; 
+  } 
 }
