1f82fa9b1e17819f4f6a3c88a4e112c47c290b79
Chichi17 <shiqi.zhang77@gmail.com>
2018-08-17 15:58:19 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var img;
 
 
 function preload() {
-  img = loadImage('DP1.png');
+//   img = loadImage('DP1.png');
   print(load);
 }
 
@@ -18,7 +18,7 @@ function setup() {
   width=400;
   height=400;
   createCanvas(width, height);
-  image(img, 0, 0);
+//   image(img, 0, 0);
 
 }
 
